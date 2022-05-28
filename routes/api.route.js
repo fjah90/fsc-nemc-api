46fb16284601f822d8acaca63cf1850393d27fd6
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-28 08:01:25 -0400

    feat: add adn fix some routes

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,18 +1,31 @@
 var express = require('express');
-var FilesController = require('../controller/files.js');
+var FilesController = require('../controllers/files.js');
 var api = express.Router();
 
-const successCallback = function(res) { return function(result) { res.json(result) }};
-const failCallback = function(res){ return function(err) {
-	console.error(err);
-	res.sendStatus(500);
-}};
+const successCallback = function (res) { return function (result) { res.json(result) } };
+const failCallback = function (res) {
+	return function (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
+};
 
 //GET
-api.get('/files', FilesController.getFiles);
-api.get('/file/:name', function (req, res) {
-	CustomerController.getCustomerById(req.params.id)
-		.then(successCallback(res), failCallback(res));
+api.get("/", (req, res) => res.json({ message: "Welcome to API!" }));
+
+api.get('/files', (req, res) => {
+	FilesController.getFiles(req, res)
+		.then(successCallback(res), failCallback(res))
+});
+
+api.get('/file/:name', (req, res) => {
+	FilesController.getFileByName(req.params.name)
+		.then(successCallback(res), failCallback(res))
+});
+
+api.get('/files/data', (req, res) => {
+	FilesController.getFormattedFiles(req, res)
+		.then(successCallback(res), failCallback(res))
 });
 
 module.exports = api;
