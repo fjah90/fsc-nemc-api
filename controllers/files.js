588ea39042348d2da8437a8ba343073f3e4b95ef
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-28 16:45:03 -0400

    fix: refact file controller

--- controllers/files.js
+++ controllers/files.js
@@ -21,6 +21,7 @@ async function getFormattedFiles() {
 		let i = 0;
 		let gulpsCVS = [];
 		while (filesName['files'][i]) {
+			// if (i >= 2) { break; }
 			//Schema
 			let newObject = {
 				"file": "",
@@ -30,31 +31,56 @@ async function getFormattedFiles() {
 			if (f !== undefined) {
 				//remove header
 				var cf = f.split("\n").filter(function (e) { return e !== 'file,text,number,hex' });
-				Object.entries(cf).forEach(item => {
-					console.log(cf[i])
-					var lines = item[1].split(",");
-					newObject["file"] = lines[0];
-					var linesFilter = lines.filter(function (e) { return e !== lines[0] });
+				console.log('---in if !undefined--------------')
+				console.info(cf)
+				if (cf.lenght !== 0) {
+					console.log('---in if !null--------------')
 
-					// console.log(linesFilter)
-					var newObjectLine = { }
-					Object.entries(linesFilter).map(iteml2 => {
-						console.log('map--------------')
-						console.log(iteml2)
-						newObjectLine["text"].push(item[0])
-						newObjectLine["number"].push(item[1])
-						newObjectLine["hex"].push(item[2][0])
+					Object.entries(cf).forEach(item => {
+						console.log('---1sf forEach--------------')
+						// console.log(item)
+
+						var lines = item[1].split(",");
+						newObject["file"] = lines[0];
+						var linesFilter = lines.filter(function (e) { return e !== lines[0] });
+
+						var newObjectLine = {
+							"text": "",
+							"number": null,
+							"hex": "",
+						}
+						console.log(linesFilter)
+						Object.entries(linesFilter).forEach((iteml2, key) => {
+							console.log('---2sf forEach--------------')
+							// console.info(iteml2[1])
+							switch (key) {
+								case 0:
+									console.info('c1 ', iteml2[1])
+									newObjectLine["text"]= iteml2[1];
+
+									break;
+								case 1:
+									console.info('c2 ', iteml2[1])
+									newObjectLine["number"] = parseInt(iteml2[1]);
+
+									break;
+								case 2:
+									console.info('c3 ', iteml2[1])
+									newObjectLine["hex"] = iteml2[1];
+									break;
+
+							}
+						})
+						newObject["lines"].push(newObjectLine)
 					})
-
-					newObject["lines"].push(newObjectLine)
-				})
+				}
 				console.log(newObject)
 
 				if (cf.lenght > 0)
 					console.log(cf[i].split(","))
 				//test if empty
 				if (cf != "")
-					gulpsCVS.push(cf);
+					gulpsCVS.push(newObject);
 			}
 			i++;
 		}
