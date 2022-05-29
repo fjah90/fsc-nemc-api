b1877d10cb1ce23e85b6128fec50937859e710e2
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-28 21:29:59 -0400

    test: add module.export to test app

--- src/index.js
+++ src/index.js
@@ -38,3 +38,5 @@ app.use(function (err, req, res, next) {
     if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
     res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
+
+module.exports = app; // for testing
