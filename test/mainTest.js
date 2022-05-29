b8134de05b7dc95f7a0558171261de3470376f1c
Fernando Aponte <fernandoaponte90@gmail.com>
2022-05-28 21:29:34 -0400

    test: add some method test

--- test/mainTest.js
+++ test/mainTest.js
@@ -1,20 +1,67 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-const expect = require('chai').expect;
+process.env.NODE_ENV = 'test';
 
-let server = require('../index.js');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../src/index');
+const should = chai.should();
 
 chai.use(chaiHttp);
 
-const url = 'http://localhost:4000';
-
 /*
-* Test the /GET files
+* Test the /GET files/list
 */
-describe('/GET files', () => {
+describe('/GET files/list', () => {
     it('it should GET all the files', (done) => {
         chai.request(server)
-            .get('/files')
+            .get('/files/list')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+});
+
+/*
+* Test the /GET /file/:name
+*/
+describe('/GET /file/:name', () => {
+    it('it should GET file by name', (done) => {
+        chai.request(server)
+            .get('/file/' + "test2.csv")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+});
+
+/*
+* Test the /GET /files/data
+*/
+describe('/GET /files/data', () => {
+    it('it should GET all the files formatting', (done) => {
+        chai.request(server)
+            .get('/files/data')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+});
+
+/*
+* Test the /GET /files/data?FileName=<FileName>
+*/
+describe('/GET /files/data?FileName=<FileName>', () => {
+    it('it should GET one file formatting', (done) => {
+        chai.request(server)
+            .get('/files/data?FileName=' + "test2.csv")
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
